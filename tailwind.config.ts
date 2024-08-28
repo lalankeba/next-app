81c6664928737cd15d3987f67375727d36bb98cd
lalankeba <lalankeba@gmail.com>
2024-08-28 11:11:46 +0530

    adding basic pages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["pastel"]
+  }
 };
 export default config;
